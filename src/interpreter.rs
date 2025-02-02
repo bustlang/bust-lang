70360163774dfc14892c9e07d743647d6204bb9f
Omri H <omri.hermon@gmail.com>
2025-02-02 14:02:57 +0200

    h

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -171,11 +171,3 @@ fn interpret_token(context: &mut Context, token: Token) {
         _ => fatal("Unknown Token Type (how did this even happen)"),
     }
 }
-
-// Stolen from StackOverflow, of course
-fn rem_first_and_last(value: &str) -> &str {
-    let mut chars = value.chars();
-    chars.next();
-    chars.next_back();
-    chars.as_str()
-}
